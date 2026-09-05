52753a08f8404b30391d5af16a8db8e3e802fa25
Zachary Brent Ramos <20187528@s.ubaguio.edu>
2026-09-05 23:39:43 +0800

    Lab2: GDScript Fundamentals and 2D Node Basics

---
+++ player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var health = 100
+
+func _ready():
+	print("Player ready. Health: ", health)
+	
+	take_damage(30)
+
+func take_damage(amount):
+	health -= amount
+	print("Ouch! Health is now: ", health)
